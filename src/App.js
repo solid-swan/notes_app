e3189aa255a464881188969920cef94fd4a1896b
Daniel de la Huerta <ddelahuerta@gmail.com>
2021-09-23 03:12:45 -0400

    setup GET api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Navigation from "./components/Navigation/Navigation";
+import Posts from "./components/Posts/Posts";
+import CardPosts from "./components/CardPosts/CardPosts";
+import SideBar from "./components/SideBar/SideBar";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navigation />
+      <Container>
+        <Row>
+          <Col>
+            <SideBar />
+          </Col>
+          <Col xs={10}>
+            <Switch>
+              <Route exact path="/apples">
+                <Posts />
+                <CardPosts />
+              </Route>
+              <Route exact path="/oranges">
+                <p>This is the oranges page</p>
+              </Route>
+            </Switch>
+          </Col>
+          <Col>3 of 3</Col>
+        </Row>
+        {/* <Row>
+          <Col>1 of 3</Col>
+          <Col xs={5}>2 of 3 (wider)</Col>
+          <Col>3 of 3</Col>
+        </Row> */}
+      </Container>
     </div>
   );
 }
--- src/components/NoteModal/NoteModal.js
+++ src/components/NoteModal/NoteModal.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
+import { Button, Form, Modal } from "react-bootstrap";
 
 class NoteModal extends Component {
   state = {
     show: false,
     notes: [],
+    message: "",
   };
 
   openModal = () => {
@@ -13,6 +13,28 @@ class NoteModal extends Component {
   };
   closeModal = () => {
     this.setState({ show: false });
+  };
+  handleMessageChange = (event) => {
+    this.setState({ message: event.target.value });
+  };
+  handleNotesApi = (event) => {
+    event.preventDefault();
+
+    fetch("https://diji-notes.herokuapp.com/notes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: this.state.message,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("this is data from the db", data);
+        this.setState({ message: "" });
+        this.closeModal();
+      });
   };
 
   render() {
@@ -32,15 +54,36 @@ class NoteModal extends Component {
             <Modal.Title>Modal title</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            I will not close if you click outside me. Don't even try to press
-            escape key.
+            <Form onSubmit={this.handleNotesApi}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Message</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Note"
+                  name={this.state.message}
+                  value={this.state.message}
+                  onChange={this.handleMessageChange}
+                />
+                {/* <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text> */}
+              </Form.Group>
+              <Modal.Footer>
+                <Button variant="danger" onClick={this.closeModal}>
+                  Cancel
+                </Button>
+                <Button variant="primary" value="submit" type="submit">
+                  Save
+                </Button>
+              </Modal.Footer>
+            </Form>
           </Modal.Body>
-          <Modal.Footer>
+          {/* <Modal.Footer>
             <Button variant="danger" onClick={this.closeModal}>
               Cancel
             </Button>
             <Button variant="primary">Save</Button>
-          </Modal.Footer>
+          </Modal.Footer> */}
         </Modal>
       </div>
     );
